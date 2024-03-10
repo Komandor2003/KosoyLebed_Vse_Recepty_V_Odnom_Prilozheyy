bcde76410be5093661393c2a07bb62bd89438614
fen1ks0059 <79647303+fen1ks0059@users.noreply.github.com>
2024-03-10 17:08:21 +0300

    Add files via upload

--- baza_django/baza_django/main/urls.py
+++ baza_django/baza_django/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.main_meals),
     path('about', views.about),
-    path('main', views.main_meals)
+    path('main', views.main_meals),
+    path('profile', views.profile),
+    path('filters', views.filters)
 ]
--- baza_django/baza_django/main/views.py
+++ baza_django/baza_django/main/views.py
@@ -4,3 +4,7 @@ def main_meals(request):
     return render(request, 'main/main_meals.html')
 def about(request):
     return render(request, 'main/about.html')
+def profile(request):
+    return render(request, 'main/profile.html')
+def filters(request):
+    return render(request, 'main/filters.html')
