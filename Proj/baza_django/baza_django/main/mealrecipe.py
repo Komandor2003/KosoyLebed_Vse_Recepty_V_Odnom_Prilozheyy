73f6674a3197c2be88ddfb2f3aa9af56cddcead8
fen1ks0059 <79647303+fen1ks0059@users.noreply.github.com>
2024-03-27 17:49:21 +0300

    Delete Proj directory

--- Proj/baza_django/baza_django/main/mealrecipe.py
+++
@@ -1,4 +0,0 @@
-import main.DB_connector as DB
-def massiv_recipe():
-    recipe_info = DB.take_meal()
-    return recipe_info
--- Proj/baza_django/baza_django/main/mytest.py
+++
@@ -1,7 +0,0 @@
-# mytest.py
-def meal():
-    return "получаю блюдо из бэкенда"
-
-
-def recipe():
-    return "получаю рецепт из бэкенда"
