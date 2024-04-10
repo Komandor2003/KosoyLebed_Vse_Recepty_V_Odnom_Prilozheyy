8dcb3b2bfd7cb0f4ccd8debff9317219718d5d99
fen1ks0059 <79647303+fen1ks0059@users.noreply.github.com>
2024-04-10 13:58:25 +0300

    Создан основной визуал страницы поиска

--- baza_django/baza_django/main/views.py
+++ baza_django/baza_django/main/views.py
@@ -1,30 +1,21 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from main.menudata import massiv
-from main.mealrecipe import massiv_recipe
-from main.massiv_poiska import massiv_poiska
-
+from main.connection_with_backend import massiv_main
+from main.connection_with_backend import massiv_recipe
+from main.connection_with_backend import massiv_poiska
+from main.connection_with_backend import find_dishes_starting_with
 def main_meals(request):
-    tmp1 = {'menuData': massiv()}
+    tmp1 = {'menuData': massiv_main()}
     return render(request, 'main/main_meals.html', tmp1)
-
 def about(request):
     return render(request, 'main/about.html')
-
 def profile(request):
     return render(request, 'main/profile.html')
-
 def filters(request):
     return render(request, 'main/filters.html')
-
 def first_meal(request):
     tmp2 = {'mealRecipe': massiv_recipe(id)}
     return render(request, 'main/first_meal.html', tmp2)
-
-def test_poiska(request):
-    # tmp3 = {'massivPoiska': massiv_poiska()}
-    return render(request, 'main/test_poiska.html')
-
 def get_recipe(request):
     index = request.GET.get('index', None)
     if index is not None:
@@ -32,8 +23,11 @@ def get_recipe(request):
         return JsonResponse(recipe_data)
     else:
         return JsonResponse({'error': 'Отсутствует параметр индекса.'}, status=400)
-
 def get_suggestions(request):
-    input_text = request.GET.get('input', '')  # Получаем ввод пользователя из GET-параметра input
-    suggestions_list = massiv_poiska(input_text)  # Получаем подсказки на основе ввода пользователя
+    input_text = request.GET.get('input', '')
+    suggestions_list = massiv_poiska(input_text)
+    return JsonResponse({'suggestions': suggestions_list})
+def get_suggestions_in_containers(request):
+    input_text = request.GET.get('input', '')
+    suggestions_list = find_dishes_starting_with(input_text)
     return JsonResponse({'suggestions': suggestions_list})
